0bb34fd0199a73ca4c8537b02d6bca01af841d6e
SirIrrational <s4920307@bournemouth.ac.uk>
2021-04-02 15:02:29 +0100

    Basic VR Setup
    Essentials for VR functionality, with 360 image testing.

---
+++ BU VR Project/Assets/Oculus Hands/Scripts/LeftHand.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class LeftHand : MonoBehaviour
+{
+    public GameObject leftHand;
+    GameObject spawnedLeftHand;
+
+    Animator leftHandAnimator;
+
+    InputDevice targetDevice;
+
+    void Start()
+    {
+        HandSpawn();
+    }
+
+
+    void Update()
+    {
+        HandFunction();
+    }
+
+    void HandSpawn()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        targetDevice = devices[0];
+        spawnedLeftHand = Instantiate(leftHand, transform);
+        leftHandAnimator = spawnedLeftHand.GetComponent<Animator>();
+    }
+
+    void HandFunction()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            leftHandAnimator.SetFloat("trigger", triggerValue);
+        }
+        else
+        {
+            leftHandAnimator.SetFloat("trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            leftHandAnimator.SetFloat("grip", gripValue);
+        }
+        else
+        {
+            leftHandAnimator.SetFloat("grip", 0);
+        }
+    }
+}
---
+++ BU VR Project/Assets/Oculus Hands/Scripts/RightHand.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class RightHand : MonoBehaviour
+{
+    public GameObject rightHand;
+    GameObject spawnedRightHand;
+
+    Animator rightHandAnimator;
+
+    InputDevice targetDevice;
+
+    void Start()
+    {
+        HandSpawn();
+    }
+
+
+    void Update()
+    {
+        HandFunction();
+    }
+
+    void HandSpawn()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        targetDevice = devices[0];
+        spawnedRightHand = Instantiate(rightHand, transform);
+        rightHandAnimator = spawnedRightHand.GetComponent<Animator>();
+    }
+
+    void HandFunction()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            rightHandAnimator.SetFloat("trigger", triggerValue);
+        }
+        else
+        {
+            rightHandAnimator.SetFloat("trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            rightHandAnimator.SetFloat("grip", gripValue);
+        }
+        else
+        {
+            rightHandAnimator.SetFloat("grip", 0);
+        }
+    }
+}
