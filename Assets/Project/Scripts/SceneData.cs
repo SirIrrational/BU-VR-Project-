54f94e703acdcab1e6f99051bc23362b93c625aa
SirIrrational <s4920307@bournemouth.ac.uk>
2021-06-29 19:14:25 +0100

    Project Fixes & Documentation
    [Mod] Scene data node change
    [Add] Project Documentation
    [Removed] Old information node material

--- Assets/Project/Scripts/SceneData.cs
+++ Assets/Project/Scripts/SceneData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Scene Node", menuName = "Scene Node")]
+[CreateAssetMenu(fileName = "Scene Data", menuName = "Scene Data")]
 public class SceneData : ScriptableObject
 {
     public string sceneName;
